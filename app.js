ebf9a8804276f234f8672735916382f4ac971e83
Ricardo Saldaña <ricardo_scuentas@outlook.com>
2025-08-05 13:12:26 -0500

    desafio contador

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 //Variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
+let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces = 'intento';
+//Bucle
+while (numeroUsuario != numeroSecreto) {
+    numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
 
+    console.log("El numero que has introducido es: " + numeroUsuario);
+    if (numeroUsuario == numeroSecreto) {
+        alert(`Felicidades, has acertado el numero secreto: ${numeroSecreto}. Lo hiciste en ${intentos} ${palabraVeces}`);
+    } else {
+        if(numeroUsuario < numeroSecreto) {
+            alert("El numero secreto es mayor que el que has introducido");
+        } else {
+            alert("El numero secreto es menor que el que has introducido");
+        }
+        //Incremento de intentos
+        intentos++;
+        palabraVeces = 'intentos';
+    }
 
-console.log("El numero que has introducido es: " + numeroUsuario);
-
-if (numeroUsuario == numeroSecreto) {
-    console.log(`Felicidades, has acertado el numero secreto: ${numeroSecreto}`);
-} else {
-    console.log("Lo siento, el numero secreto es otro");
-}
+} 
---
+++ contador.js
@@ -0,0 +1,18 @@
+let contadorUp = 0;
+let contadorDown = 10;
+
+function contarProgresivo() {
+    contadorUp++;
+    console.log("Contador: " + contadorUp);
+}
+
+function contarRegresivo() {
+    contadorDown--;
+    console.log("Contador: " + contadorDown);
+}
+
+while (contadorUp < 10 && contadorDown > 0) {
+    alert("Contador progresivo: " + contadorUp + "\nContador regresivo: " + contadorDown);
+    contarProgresivo();
+    contarRegresivo();
+}
