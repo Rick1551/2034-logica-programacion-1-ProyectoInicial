06f2204ba01fd9279aee1482f2c571f9cbb912e4
Ricardo Saldaña <ricardo_scuentas@outlook.com>
2025-08-05 12:39:03 -0500

    if

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert("Hello, World!");
+//Variables
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
+
+
+console.log("El numero que has introducido es: " + numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    console.log(`Felicidades, has acertado el numero secreto: ${numeroSecreto}`);
+} else {
+    console.log("Lo siento, el numero secreto es otro");
+}
